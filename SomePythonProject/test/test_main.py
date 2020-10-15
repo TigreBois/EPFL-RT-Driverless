e496ec6828ed29fdf629da652f974300e15c5b8e
TigreBois <matthieu.masouye@epfl.ch>
2020-10-15 10:44:56 +0200

    Removed c++ project

--- SomePythonProject/test/test_main.py
+++ SomePythonProject/test/test_main.py
@@ -11,9 +11,7 @@ class ExampleTest(unittest.TestCase):
     def testMain_right(self):
         self.assertEqual(ex.main(),"Hello World!")
 
-    #shouldn't pass
-    def testMain_wrong(self):
-        self.assertEqual(ex.main(),"hElLo wOrLd!")
+    
 
 if __name__ == '__main__':
     unittest.main()
