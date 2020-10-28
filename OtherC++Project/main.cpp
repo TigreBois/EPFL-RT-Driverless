b200a45557e1590855969582350668365ee3602d
TigreBois <matthieu.masouye@epfl.ch>
2020-10-28 14:22:33 +0100

    Adding another c++ project for testing coverage

---
+++ OtherC++Project/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int GreatestOfThree(int a,int b,int c){
+  if((a>b) && (a>c)){    //for a > b and a>c case
+    return a;
+   }
+   else if(b>c){    //for b>c case 
+    return b;
+   }
+  else{
+    return c;
+  }
+ return 0;
+}
+//After checking for correctness comment main function to test //main_test.cpp
+int main()
+{
+ // your code goes here
+ int a,b,c;
+ cin>>a>>b>>c;
+ cout<<GreatestOfThree(a,b,c);
+ return 0;
+}
