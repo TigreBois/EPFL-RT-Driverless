3ca2a25b1528fed9e8db9f085e67c2243b3aa96e
Oganes Manasian <oganesman8@gmail.com>
2021-03-02 10:42:30 +0100

    Adapted simulation for real model of the vehicle

--- Simulation/Webots environment/controllers/car_controller/car_controller.py
+++ Simulation/Webots environment/controllers/car_controller/car_controller.py
@@ -11,11 +11,10 @@ from cones import Cones
 from coordstransformslocal import webots2ISO, world2car
 
 # SENSORS_DATA_SAVING_TIME_INTERVAL = 0.1 # in seconds
-SENSORS_DATA_FILENAME = "circle_track_sensors_data"
-MAP_SCALING = 100
+SENSORS_DATA_FILENAME = "sensors_data"
 
-CONE_SCALING = 0.06
-CONE_PERCEPTION_DISTANCE = 0.7 # in meters
+CONE_SCALING = 0.5 # How cones were scaled when created (Will be used to ease search of cones in Webots window)
+CONE_PERCEPTION_DISTANCE = 20 # in meters
 DO_PERCEPTION = False
 BASE_SPEED = 2.0
 
@@ -23,41 +22,41 @@ BASE_SPEED = 2.0
 # We will store sensors data here
 sensors_data = []
 
+# Get cone coordinates
+cones = Cones("../../../cone_coordinates.csv")
+
 # create the Robot instance.
 robot = Robot()
-
-# Get cone coordinates
-cones = Cones("circle_track_cones_coordinates.csv")
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
 # You should insert a getDevice-like function in order to get the
-motors = []
-wheels_names = ["wheel1", "wheel2", "wheel3", "wheel4"]
-for name in wheels_names:
-    motor = robot.getMotor(name)
-    motor.setPosition(float('inf'))
-    motor.setVelocity(0.0)
-    motors.append(motor)
+# motors = []
+# wheels_names = ["wheel1", "wheel2", "wheel3", "wheel4"]
+# for name in wheels_names:
+    # motor = robot.getMotor(name)
+    # motor.setPosition(float('inf'))
+    # motor.setVelocity(0.0)
+    # motors.append(motor)
 
 # Activate sensors
-lidar = robot.getLidar("lidar")
+lidar = robot.getDevice("lidar")
 lidar.enable(timestep) # Are you sure?
 lidar.enablePointCloud()
 
-gps = robot.getGPS("gps")
+gps = robot.getDevice("gps")
 gps.enable(timestep)
 
-gyro = robot.getGyro("gyro")
+gyro = robot.getDevice("gyro")
 gyro.enable(timestep)
 # gyro.xAxis, gyro.zAxis = False, False # We need only yaw rate
 
-imu = robot.getInertialUnit("imu")
+imu = robot.getDevice("imu")
 imu.enable(timestep)
 # imu.xAxis, imu.zAxis = False, False # We need only yaw angle
 
-accelerometer = robot.getAccelerometer("accelerometer")
+accelerometer = robot.getDevice("accelerometer")
 accelerometer.enable(timestep)
 
 # Main loop:
@@ -101,80 +100,48 @@ while robot.step(timestep) != -1:
     yaw_rate = angle_velocity[1]
     print("Yaw rate is", yaw_rate)
     
-    # cones_rel_coords = cones.get_cones_relative_coordinates(robot_coord)
-    # print("Cone relative coordinates:")
-    # cones_rel_coords_human_readable = [(cone[0], cone[1], cone[2],
-                                        # cone[3] / CONE_SCALING, 
-                                        # cone[4] / CONE_SCALING) for cone in cones_rel_coords]
-    # for cone in cones_rel_coords_human_readable:
-        # print(cone)
-        
-    # cones_within_dist = cones.get_cones_within_distance(robot_coord, CONE_PERCEPTION_DISTANCE)
-    # print(f"Cone within {CONE_PERCEPTION_DISTANCE} distance:")
-    # cones_within_dist_human_readable = [(cone[0], cone[1], cone[2],
-                                        # cone[3] / CONE_SCALING, 
-                                        # cone[4] / CONE_SCALING) for cone in cones_within_dist]
-    # for cone in cones_within_dist_human_readable:
-        # print(cone)
-        
-    # cones_within_dist_in_front = cones.get_cones_within_dist_in_front(robot_coord, yaw[2], CONE_PERCEPTION_DISTANCE)
-    # print(f"Cone within {CONE_PERCEPTION_DISTANCE} distance in front of robot:")
-    # cones_within_dist_in_front_human_readable = [(cone[0], cone[1], cone[2],
-                                        # cone[3] / CONE_SCALING, 
-                                        # cone[4] / CONE_SCALING) for cone in cones_within_dist_in_front]
-    # for cone in cones_within_dist_in_front_human_readable:
-        # print(cone)
-        
-    # rel_cones = cones.get_cones_in_front_car(robot_coord, yaw[2], CONE_PERCEPTION_DISTANCE)
-    # print(f"Cone within {CONE_PERCEPTION_DISTANCE} distance in front of robot:")
-    # for cone in rel_cones:
-        # print(cone)
-
+    
     robot_coord_ISO = webots2ISO(robot_coord)
-    print("Robot coords ISO", robot_coord_ISO)
+    print("Robot coords ISO:", robot_coord_ISO)
+    # yaw_ISO = yaw + pi/2 # For previous version of car (New version is rotated)
     yaw_ISO = yaw + pi/2
-    print("yaw ISO", yaw_ISO)
+    print("yaw ISO:", yaw_ISO)
     robot_orientation_ISO = (0, 0, yaw_ISO)
     cones_car_ISO = cones.get_cones_car_ISO_coords(robot_coord_ISO, robot_orientation_ISO)
-    # print("Cones in car ISO coordinates")
     cones_car_ISO = [(cone[0], cone[1], cone[2], cone[3],
-                          cone[4] / CONE_SCALING, cone[5] / CONE_SCALING) for cone in cones_car_ISO]
-    # for cone in cones_car_ISO:
-        # print(cone)
+                      cone[4] / CONE_SCALING, cone[5] / CONE_SCALING) for cone in cones_car_ISO]
+    print("Cones in car ISO coordinates:")
+    for cone in cones_car_ISO:
+        print(cone)
         
-    
     cones_within_dist = [cone for cone in cones_car_ISO
         if 0 < cone[1] < CONE_PERCEPTION_DISTANCE and -CONE_PERCEPTION_DISTANCE < cone[2] < CONE_PERCEPTION_DISTANCE]
     print("Visible cones in car ISO coordinates")
     for cone in cones_within_dist:
         print(cone)
         
-    sensors_data_new = [robot_coord_ISO[0] * MAP_SCALING, 
-                        robot_coord_ISO[1] * MAP_SCALING, 
-                        robot_speed * MAP_SCALING,
-                        acceleration_dir * MAP_SCALING**2,
+    sensors_data_new = [robot_coord_ISO[0], 
+                        robot_coord_ISO[1], 
+                        robot_speed,
+                        acceleration_dir ,
                         yaw_ISO, yaw_rate, 
-                        [(cone[0], cone[1] * MAP_SCALING,
-                                   cone[2] * MAP_SCALING) for cone in cones_within_dist]]
+                        [(cone[0], cone[1], cone[2]) for cone in cones_within_dist]]
                                                 
     sensors_data.append(tuple(sensors_data_new))
     
     # Enter here functions to send actuator commands, like:
-    for i, motor in enumerate(motors):
-        coef = 1 if i % 2 == 0 else 0.8
-        motor.setVelocity(coef * BASE_SPEED)
+    # for i, motor in enumerate(motors):
+        # coef = 1 if i % 2 == 0 else 0.8
+        # motor.setVelocity(coef * BASE_SPEED)
         
     step += 1
     
-print("Saving results")
+# Enter here exit cleanup code.
+
 # Save sensors data as .csv and .pickle file
+print("Saving results")
 with open(SENSORS_DATA_FILENAME + ".pickle", "wb") as f: # save list of tuples as .pickle
     pickle.dump(sensors_data, f)
     
-# sensors_data_pd = pd.DataFrame(sensors_data, columns=["x", "y", "v",
-                                                      # "yaw angle", "yaw rate", "cones"])
-# sensors_data_pd.to_csv(SENSORS_DATA_FILENAME + ".csv", index=False) # save as .csv
-
-# Enter here exit cleanup code.
 
 # TODO do we need to disable lidar, gps, robot here?
