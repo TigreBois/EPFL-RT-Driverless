3b050487f7c69ffa30b12598ba1757e70d32f8c0
Marcel Torne <marcel.tornevillasevil@epfl.ch>
2021-03-16 15:35:29 +0100

    Fix problems with Open AI gym environment

--- Simulation/Webots environment/controllers/roboSupervisorController/roboSupervisorController.py
+++ Simulation/Webots environment/controllers/roboSupervisorController/roboSupervisorController.py
@@ -6,13 +6,14 @@ from gym.spaces import Box, Discrete, Dict
 from vehicle import Driver
 import numpy as np
 from math import inf
+
 import gym
 
 START_THRESHOLD = 5 # in seconds. Max time to start the car.
 
 env = gym.make("gym_race:race-v0")
 print(env)
-agent = PPOAgent(numberOfInputs=env.observation_space.shape[0], numberOfActorOutputs=env.action_space.shape[0])
+#agent = PPOAgent(numberOfInputs=env.observation_space.shape[0], numberOfActorOutputs=env.action_space.shape[0])
 
 solved = False
 
@@ -26,20 +27,21 @@ while not solved and episodeCount < episodeLimit:
     
     for step in range(env.stepsPerEpisode):
         # In training mode the agent samples from the probability distribution, naturally implementing exploration
-        selectedAction, actionProb = agent.work(observation, type_="selectAction")
-        
+        #selectedAction, actionProb = agent.work(observation, type_="selectAction")
+        selectedAction = [5,0]
         # Step the supervisor to get the current selectedAction's reward, the new observation and whether we reached 
         # the done condition
-        newObservation, reward, done, info = env.step([selectedAction])
+        
+        newObservation, reward, done, info = env.step(selectedAction)
 
         # Save the current state transition in agent's memory
-        trans = Transition(observation, selectedAction, actionProb, reward, newObservation)
-        agent.storeTransition(trans)
+        #trans = Transition(observation, selectedAction, actionProb, reward, newObservation)
+        #agent.storeTransition(trans)
         
         if done:
             # Save the episode's score
             env.episodeScoreList.append(env.episodeScore)
-            agent.trainStep(batchSize=step)
+            #agent.trainStep(batchSize=step)
             solved = env.solved()  # Check whether the task is solved
             break
 
@@ -54,6 +56,6 @@ if not solved:
 elif solved:
     print("Task is solved, deploying agent for testing...")
 observation = env.reset()
-while True:
-    selectedAction, actionProb = agent.work(observation, type_="selectActionMax")
-    observation, _, _, _ = env.step([selectedAction])
+#while True:
+    #selectedAction, actionProb = agent.work(observation, type_="selectActionMax")
+    #observation, _, _, _ = env.step([selectedAction])
