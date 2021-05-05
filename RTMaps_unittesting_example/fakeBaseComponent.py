b4a3a7e888d524abefcf97b89f5805f075ea1acb
TigreBois <matthieu.masouye@epfl.ch>
2021-05-05 16:11:58 +0200

    Pushing testing script template for testing modules; still need to implemetn timeouts

---
+++ RTMaps_unittesting_example/fakeBaseComponent.py
@@ -0,0 +1,15 @@
+
+class BaseComponent():
+    
+    inputs = {"in" : 0}
+    outputs = {}
+
+    def __init__(self):
+        return
+    
+    def add_input(self):
+        return
+        
+    def add_output(self):
+        return
+        
---
+++ RTMaps_unittesting_example/fakeRTMaps.py
@@ -0,0 +1,28 @@
+class core():
+    def __init__(self):
+        return
+
+class types():
+    def __init__(self):
+        return
+        
+class reading_policy():
+    def __init__(self):
+        return
+
+class base_component():
+    class BaseComponent():
+        
+        inputs = {"in" : 0}
+        outputs = {}
+
+        def __init__(self):
+            return
+        
+        def add_input(self):
+            return
+            
+        def add_output(self):
+            return
+    def __init__(self):
+        return
